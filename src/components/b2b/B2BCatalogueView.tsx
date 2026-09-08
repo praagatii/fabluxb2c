7963dea64fd0fef0e2dbbccb25d86bb94d2cb582
you <you@example.com>
2026-09-08 12:47:05 +0530

    Bulletproof hidden-scrollbar class for filter panes

--- src/components/b2b/B2BCatalogueView.tsx
+++ src/components/b2b/B2BCatalogueView.tsx
@@ -135,7 +135,7 @@ export function B2BCatalogueView({
   return (
     <div className="grid gap-8 lg:grid-cols-[16rem_1fr]">
       <aside className="hidden lg:block">
-        <div className="sticky top-24 max-h-[calc(100dvh-7.5rem)] overflow-y-auto [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+        <div className="scrollbar-hide sticky top-24 max-h-[calc(100dvh-7.5rem)] overflow-y-auto">
           {filters}
         </div>
       </aside>
